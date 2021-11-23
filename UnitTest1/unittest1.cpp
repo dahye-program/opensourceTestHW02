7873591ac9abe7e820a6262fd09a519d57a310d2
dahye-program <dahye0729@sch.ac.kr>
2021-11-23 19:13:36 +0900

    단위테스트 작성

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -1,5 +1,14 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "../opensourceTestHW02/Triangle.cpp"
+
+bool operator== (struct Triangle t1, struct Triangle t2) {
+	return t1.area == t2.area;
+}
+
+std::wstring ToString(struct Triangle t) {
+	return std::to_wstring(t.area) + std::wstring(L"+");
+}
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,9 +18,15 @@ namespace UnitTest1
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TriangleArea)
 		{
-			// TODO: 테스트 코드를 여기에 입력합니다.
+			struct Triangle t, result, compare;
+			t.width = 3;
+			t.height = 4;
+
+			result = Triangle_area(t);
+			compare.area = 6;
+			Assert::AreEqual(result, compare);
 		}
 
 	};
